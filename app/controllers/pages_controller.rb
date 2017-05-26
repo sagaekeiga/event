a24677f85a0bbb27e1cb42af21783f4e40e16049
sagae keiga <sagae5.28rujeae@gmail.com>
2017-05-26 16:19:35 +0000

    g

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,56 @@ class PagesController < ApplicationController
     @q = Event.search(params[:q])
   end
   
+  def entertainment
+    @events = Event.all
+    @q = Event.search(params[:q])
+  end
+  
+  def sport
+    @events = Event.all
+    @q = Event.search(params[:q])
+  end
+  
+  def business
+    @events = Event.all
+    @q = Event.search(params[:q])
+  end
+  
+  def life
+    @events = Event.all
+    @q = Event.search(params[:q])
+  end
+  
+  def art
+    @events = Event.all
+    @q = Event.search(params[:q])
+  end
+  
+  def internet
+    @events = Event.all
+    @q = Event.search(params[:q])
+  end
+  
+  def political
+    @events = Event.all
+    @q = Event.search(params[:q])
+  end
+  
+  def local
+    @events = Event.all
+    @q = Event.search(params[:q])
+  end
+  
+  def fashion
+    @events = Event.all
+    @q = Event.search(params[:q])
+  end
+  
+  def other
+    @events = Event.all
+    @q = Event.search(params[:q])
+  end
+  
       private
 
         def detect_variant
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,17 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   get 'events/manage'
+  
+  get 'pages/entertainment'
+  get 'pages/sport'
+  get 'pages/business'
+  get 'pages/art'
+  get 'pages/internet'
+  get 'pages/political'
+  get 'pages/local'
+  get 'pages/fashion'
+  get 'pages/other'
+  
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
